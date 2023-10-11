4cc974660196c007fcc72771828fc343ab4f2822
Brad Smith <bradsmith@duck.com>
2023-10-11 10:23:08 -0400

    Give up on data-vals, update colors, add (in).active CSS

--- js/app.js
+++ js/app.js
@@ -5,11 +5,12 @@ const COLORS = {
   0: '-',               // null. not valid
   1: 'crimson',         // used as 'exact' in results
   2: 'white',           // used as 'partial' in results
-  3: 'darkslategray',   // used as 'wrong' in results
-  4: 'cadetblue',
+  3: 'black',   // used as 'wrong' in results
+  4: 'blue',
   5: 'green',
   6: 'goldenrod',
 }
+
 const CODE_LENGTH = 4;
 const MAX_ROUNDS = 10;
 
@@ -21,13 +22,13 @@ let codeToBreak;    // [x,x,x,x]. 4 randoms from COLORS
 let currentRound;   // #. of 10 rows/guesses
 let roundResults;   // []. 'wrong', 'exact', 'partial'
 let currentGuess;   // [x,x,x,x]. 4 numbers that equate to COLORS[x]
+let currentCell;
 
 
-/* =======================
-/* =======================
+/* =====================================================================
 /* cached elements 
-/* =====================*/
-/* =====================*/
+/* ===================================================================*/
+
 
 /* =======================
 /* the entire board
@@ -47,10 +48,10 @@ const currentRoundEl = document.querySelector('#message');
 // all rows; ∑ should be 20
 const allRowEls = document.querySelectorAll('#gameBoard .row');
 
-// active *guess* row ; ∑ should be 4 cells
+// cells in active *guess* row ; ∑ should be 4 cells
 const activeGuessEls = document.querySelectorAll('#guessCells .row.active .cell');
 
-// active *results* row; ∑ should be 4 cells
+// cells in active *results* row; ∑ should be 4 cells
 const activeResultEls = document.querySelectorAll('#resultCells .row.active .cell');
 
 // both .active rows (∑2: one active #guessCells row, one active #resultCells row)
@@ -87,86 +88,6 @@ resetEl.addEventListener('click', init);
 /* =====================*/
 init();
 
-// check current guess (2 arrays: the guess and the answer)
-function handleGuessCheck() {
-  activeGuessEls.forEach((cell, idx) => {
-    currentGuess[idx] = `${cell.dataset.value}`;
-    // currentGuess[idx] = cell.innerText;
-  });
-  return checkGuess(currentGuess);
-}
-
-// actually check current guess against codeToBreak
-function checkGuess() {
-
-  // clear roundResults
-  roundResults = [];
-
-  // GUARD
-  // only check once currentGuess is valid
-  if (currentGuess.includes('0')) {
-    renderResultMessage("That's not a valid guess");
-    return;
-  } else {
-    let valuesChecked = [];
-    currentGuess.forEach((cell, idx) => {
-      // if current cell is wrong
-      if (!codeToBreak.includes(cell.toString())) {
-        // console.log(cell);
-        roundResults.push('wrong');
-        // …if it's exactly right
-      } else if (cell.toString() === codeToBreak[idx]) {
-        // console.log(`EXACTAMUNDO! ${cell}`);
-        roundResults.push('exact');
-        // if it's there but not in that index
-      } else if (codeToBreak.includes(cell.toString()) && !valuesChecked.includes(cell)) {
-        roundResults.push('partial');
-        valuesChecked.push(cell);
-      }
-    })
-    console.log(roundResults);
-  }
-  // handle winner (.exact = 4);
-  if (roundResults.every(val => val === 'exact')) {
-    console.log("YOU WON!");
-    renderAnswer();
-  };
-
-  // update results row
-  renderResultsRow(roundResults);
-
-  // update round (message);
-  renderRound()
-
-  // update active rows
-  // updateActiveRows();
-
-  return;
-}
-
-// click through each available color for each cell
-function handleNewColor(evt) {
-
-  // GUARDS
-  // ignore if the actual cell wasn't clicked
-  if (!evt.target.classList.contains('cell')) { return };
-
-  console.log(typeof evt.target.dataset.value);
-
-  // // increment clicks unless at end (then start over)
-  // if (something is < Object.keys(COLORS).length - 1) {
-  //   // evt.target.style.backgroundColor = `${COLORS[evt.target.dataset.value]}`;
-  //   // cycle through values
-  // } else {
-  //   // (if value is 6, start over)
-  // }
-}
-
-// change return value to next value in COLORS
-// function updateColor(colorKey) {
-//   console.log(`color needs to change to: ${COLORS[colorKey]}…`)
-// }
-
 function init() {
   // (re)set all state
   currentRound = 0;
@@ -188,7 +109,7 @@ function init() {
   currentGuess = [0, 0, 0, 0];
   roundResults = [];
   console.log(`It's round ${currentRound}`)
-  console.log(`Code (number): ${codeToBreak}`)
+  console.log(`Answer: ${codeToBreak}`)
 
   // re-enable checkGuess button
   checkGuessEl.removeAttribute('disabled');
@@ -197,8 +118,6 @@ function init() {
   resetEl.innerText = "Reset";
 
   // reset rows
-
-
 
 
   activeGuessEls.forEach((cell, idx) => {
@@ -217,6 +136,99 @@ function init() {
   render();
 }
 
+// check current guess (2 arrays: the guess and the answer)
+function handleGuessCheck() {
+  activeGuessEls.forEach((cell, idx) => {
+    currentGuess[idx] = cell.innerText;
+  });
+
+  return checkGuess(currentGuess);
+}
+
+// actually check current guess against codeToBreak
+function checkGuess() {
+
+  // add guess to board
+  console.log(`currentRound: ${currentRound}`)
+  if (currentRound === 1) {
+    board[currentRound-1] = currentGuess;
+  } else {
+    board[currentRound] = currentGuess;
+  }
+
+  // clear roundResults
+  roundResults = [];
+
+  // GUARD
+  // only check once currentGuess is valid
+  if (currentGuess.includes('-')) {
+    renderResultMessage("That's not a valid guess");
+    return;
+  } else {
+    let valuesChecked = [];
+    currentGuess.forEach((cell, idx) => {
+      // if current cell is wrong
+      if (!codeToBreak.includes(cell.toString())) {
+        // console.log(cell);
+        roundResults.push('wrong');
+        // …if it's exactly right
+      } else if (cell.toString() === codeToBreak[idx]) {
+        // console.log(`EXACTAMUNDO! ${cell}`);
+        roundResults.push('exact');
+        // if it's there but not in that index
+      } else if (codeToBreak.includes(cell.toString()) && !valuesChecked.includes(cell)) {
+        roundResults.push('partial');
+        valuesChecked.push(cell);
+      }
+    })
+    console.log(roundResults);
+  }
+
+  // handle winner (.exact = 4);
+  if (roundResults.every(val => val === 'exact')) {
+    renderAnswer("YOU WON!");
+  };
+
+  // can i update the guess board with each round of guesses?
+
+  // [...board[currentRound]].map((val, idx) => board[currentRound][idx] = currentGuess[idx]);
+
+  // update results row
+  renderResultsRow(roundResults);
+
+  // update round (message);
+  renderRound()
+
+  // update active rows
+  // updateActiveRows();
+
+  return;
+}
+
+// click through each available color for each cell
+function handleNewColor(evt) {
+  console.log(`${evt.target.innerText} (before loop)`);
+
+  // GUARDS
+  // ignore if the actual cell wasn't clicked
+  if (!evt.target.classList.contains('cell')) { return };
+
+  // cycle through clicks/colors, looping at end
+  if (evt.target.innerText === '-') {
+    evt.target.innerText = '1';
+    // evt.target.style.backgroundColor = `${COLORS[1]}`
+    // evt.target.style.color = `${COLORS[1]}`
+  } else if (evt.target.innerText === '6') {
+    evt.target.innerText = '1';
+    // evt.target.style.backgroundColor = `${COLORS[6]}`
+    // evt.target.style.color = `${COLORS[6]}`
+  } else {
+    evt.target.innerText++;
+    // evt.target.style.backgroundColor = `${COLORS[evt.target.innerText]}`
+    // evt.target.style.color = `${COLORS[evt.target.innerText]}`
+  }
+}
+
 function toggleCheckButton() {
   // if button is currently disabled, re-enable it
   if (checkGuessEl.getAttributeNames().includes('disabled')) {
@@ -233,7 +245,6 @@ function toggleCheckButton() {
 function render() {
   renderRound(currentRound);
   renderBoard();
-
 }
 
 function renderBoard() {
@@ -244,16 +255,18 @@ function renderBoard() {
     cell.style.color = 'initial';
   });
 
-
-
   theAnswerEls.forEach(cell => {
     cell.innerText = `?`;
     cell.style.backgroundColor = `initial`;
     cell.style.borderColor = `${COLORS[6]}`;
   });
+
   resultMsgEl.innerText = "Let's play!"
 
   // reset 'active' class on guess row and result row
+  allRowEls.forEach(row => row.classList.remove('active'));
+  allRowEls[0].classList.add('active');
+  allRowEls[10].classList.add('active');
 
   return board;
 }
@@ -263,29 +276,31 @@ function renderRound() {
   // GUARD. 
   // if you lose on the last round, it's GAME OVER:
   if (currentRound === MAX_ROUNDS) {
-    currentRoundEl.innerHTML = "Sorry, You Lose";
-    resultMsgEl.innerText = "Want to try again?";
-    // change "what round?" text
-    // show answer
-    renderAnswer();
-    // change "reset button" text
-    resetEl.innerText = "Play again";
+    renderAnswer("Sorry, You Lose");
     // toggle "check guess" button
     toggleCheckButton();
     // just update current round 
   } else {
-    currentRoundEl.innerHTML = (`Round <mark>${++currentRound}</mark> of 10`);
+    currentRoundEl.innerHTML = (`Round ${currentRound + 1} (actual: ${currentRound}) of ${MAX_ROUNDS}`);
+    currentRound += 1;
     renderResultMessage("Not quite—try again!");
   }
 }
 
-// render answer
-function renderAnswer() {
-  // theAnswerEls.visibility = 
+// render answer  
+function renderAnswer(message) {
+  // show a message instead of "round x of 10"
+  console.log(message);
+  currentRoundEl.innerText = message;
+  resultMsgEl.innerText = "Want to try again?";
+
+  // change "reset button" text
+  resetEl.innerText = "Play again";
+
   theAnswerEls.forEach((cell, idx) => {
     // cell.style.backgroundColor = `${codeToBreak[idx]}`;
     cell.style.backgroundColor = `${COLORS[codeToBreak[idx]]}`;
-    cell.style.borderColor = (`${COLORS[codeToBreak[idx]]} === 'white'`) ? 'black' : `${COLORS[codeToBreak[idx]]}`;
+    cell.style.borderColor = `${COLORS[codeToBreak[idx]]}`;
     cell.innerText = '';
   })
 }
